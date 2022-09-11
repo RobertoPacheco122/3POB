1ebc490e3767537462d0e71c4c18892deeea326d
Roberto Pacheco <Robertoaruajo230@hotmail.com>
2022-09-11 18:02:17 -0300

    Criando arquivo e lógica da classe Disciplina

---
+++ trabalho2/src/main/java/entities/Disciplina.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class Disciplina {
+    private String name;
+    private int code;
+    
+    public Disciplina(String name, int code){
+        this.code = code;
+        this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }   
+}
